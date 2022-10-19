3c371ed8e0a17d3e0a793d9f616139c766870f36
JaimeCarcamoSepulveda <jaimecarcamobroker@gmail.com>
2022-10-18 23:53:20 -0700

    Add background image, logo, revise html and script
    make background image fix and center it. make "California-2-Chile" logo
    give navbar defined height. Revise script code—use ES6 JS destructuring
    create object of all referenced unit measurement inputs.
    Style page with center container add padding to form and size for inputs
    style disclosure section and revise semantics of html

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 /*
 FUNCTIONS TO BUILD
-
-1st-feet to meters,
-2nd- miles to kilometers,
-3rd -degrees to Fahrenheit, 
-4th- gasoline gallons to liters
 5th--*shoe size converter
+6th *season converter (if its spring in Chile its... blank in (INPUT STATE))
+gas price in real-time chile
+USD to Clp exchange rate in real-time 
 
-*season converter (if its spring in Chile its... blank in (INPUT STATE))
-
-**dollar to peso 
 
 
 */
+
+document.querySelector(`[type="submit"]`).addEventListener("click", response);
+let formInput = {}
+
+function response(e) {
+    e.preventDefault()
+    let name = document.querySelector(`[id="name"]`).value;
+    let homeState = document.querySelector(`[id="homeState"]`).value;
+
+    let USShoeSize = document.querySelector(`[id="ShoeSize"]`).value;
+    let height = document.querySelector(`[id="height"]`).value;
+    let waist = document.querySelector(`[id="waist"]`).value;
+    let carSize = document.querySelector(`[id="carType"]`).value;
+    let cash = document.querySelector(`[id="cash" ]`).value;
+    let gender = document.querySelector(`[id="gender"]`).value;
+   let { name: yourName, homeState: yourHomeState, height: yourHeight, carSize: yourCarSize, cash: yourCash, gender: yourGender, USShoeSize: yourShoeSize, waist: yourWaist } = formInput;
+
+    //let yourHeight = feetToMeters(height);
+   // let yourShoeSize = USShoeSize(USShoeSize)
+   
+        console.log(`you are a ${gender}`)
+
+    //const gender = document.querySelector(``).value;
+    
+    //console.log(`hello ${name} from ${homeState} with shoe size of ${USShoeSize} with height of ${height} feet and available spending cash of ${cash}`);
+
+}
+
+//returns adjusted input for correct gender
+/*
+getGender(yourGender){
+    if(yourGender == "female") {
+       yourShoeSize = womenShoeSize;
+       waist = womenWaist; 
+    }
+};
+*/
+
 
 
 /*Takes in ft or meters and returns the other. 
@@ -38,7 +71,6 @@ function milesToKilometers(miles=``, kilometer = ``) {
         return `${kilometer*0.621371} miles`;
     } else return;
 }
-
 
 /* Fahrenheit to celsius formula == f - 32* .5556 (OR 5/9)
 Celsius to Fahrenheit formula == c *1.8(OR 9/5) + 32 */
